6888ef31269de221540052881f2367abc71bc9dc
Darren Embry <dse@webonastick.com>
2025-09-13 01:27:58 -0400

    silence(None) to disable forever

--- lib/silence.py
+++ lib/silence.py
@@ -1,8 +1,15 @@
 import os
 stderr_fd = os.dup(2)
+disabled = False
 def silence(flag=True):
+    global disabled
     global stderr_fd
-    if flag:
+    if disabled:
+        return
+    elif flag is None:
+        disabled = True
+        os.dup2(stderr_fd, 2)
+    elif flag:
         os.close(2)
     else:
         os.dup2(stderr_fd, 2)
