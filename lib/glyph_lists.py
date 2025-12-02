0296041547de44f1c0569a727d26eaa94ddeb0c2
Darren Embry <dse@webonastick.com>
2025-12-02 02:14:43 -0500

    glyph lists

---
+++ lib/glyph_lists.py
@@ -0,0 +1,14 @@
+__all__ = [
+    "WGL4",
+    "AGLFN",
+    "AGL",
+    "W1G",
+    "WGL4_PRIVATE_USE",
+    "ZAPF_DINGBATS",
+]
+
+from glyph_list_wgl4          import WGL4, WGL4_PRIVATE_USE
+from glyph_list_w1g           import W1G
+from glyph_list_aglfn         import AGLFN
+from glyph_list_agl           import AGL
+from glyph_list_zapf_dingbats import ZAPF_DINGBATS
